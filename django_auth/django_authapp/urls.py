d6929b98643d1cdf16fb3b9bcc02a2955baf17d7
swati pushp <pushpswati@gmail.com>
2018-08-12 01:10:51 +0530

    first commit

---
+++ django_auth/django_authapp/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from .views import CreateUserAPIView
+ 
+urlpatterns = [
+    url(r'^create/$', CreateUserAPIView.as_view()),
+    url(r'^update/$', UserRetrieveUpdateAPIView.as_view()),
+]
